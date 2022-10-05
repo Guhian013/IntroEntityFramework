d1abda7fd1eee02d17412c860dca257970c399a3
Gustavo Ian <84183686+Guhian013@users.noreply.github.com>
2022-10-05 11:42:52 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using IntroEntityFramework.Models;
+
+SystemContext context = new SystemContext();
+context.Database.EnsureCreated();
+
+// Computer c1 = new Computer(1, "2gb", "i3");
+// context.Computers.Add(c1);
+// context.SaveChanges();
+
+// Computer c2 = new Computer(2, "4gb", "i5");
+// context.Computers.Add(c2);
+// context.SaveChanges();
+
+// Computer c3 = new Computer(3, "8gb", "i7");
+// context.Computers.Add(c3);
+// context.SaveChanges();
+
+IEnumerable<Computer> computers = context.Computers.ToList();
+
+foreach (var item in computers)
+{
+    Console.WriteLine($"{item.Id}, {item.Ram}, {item.Processor}");
+}
+
+Computer encontrado = context.Computers.Find(2);
+Console.WriteLine($"{encontrado.Id}, {encontrado.Ram}, {encontrado.Processor}");
+
+encontrado.Ram = "20gb";
+encontrado.Processor = "amd";
+
+context.Computers.Update(encontrado);
+context.SaveChanges();
+
+context.Computers.Remove(encontrado);
+context.SaveChanges();
